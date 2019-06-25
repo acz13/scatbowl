cd6b3659babe85688015f12466095d2c540cad83
biz11 <biz11@scasd.org>
2019-06-25 13:32:52 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,33 +4,48 @@ const http = require('http').Server(app);
 const io = require('Socket.io')(http)
 const path = require('path');
 
+const clients=[];
 let correctAnswer='';
 const scores=[];
-//Currently this is just a random preset, would be cleaner to have it actually get the first connection's name
 let scoresID=0;
 
+const buzzes=[];
+let currentPlaces=[];
+let canBuzz=true;
+let startWord;
+let delayTime;
+let currentQuestion='';
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, + 'public/index.html'))
 });
 
-
+const nsp = io.of('/my-namespace');
+nsp.on('connection', function(socket){
+    console.log('does this work');
+  });
 function legalName(name){
-    console.log("oof " + JSON.stringify(scores));
     for (scoreItem of scores){
-        console.log("this happening" + JSON.stringify(scoreItem));
         if (scoreItem.player==name){
-            console.log("it's illegal!");
             return(false);
         }
     }
     return(true);
 }
-
+function getAverage(){
+    let total=0;
+    for(place of currentPlaces){
+        total+=place;
+    }
+    return(total/currentPlaces.length);
+}
 io.on('connection', (socket) => {
-    console.log("a user connected");
-    console.log("current players..." + JSON.stringify(scores));
+    if(currentQuestion==''){
+        fetchNewQuestion();
+    }
+    clients.push(socket.id);
+
     let genNewName = "SCAT";
     let z = 1;
     while (!legalName(genNewName)){
@@ -43,36 +58,71 @@ io.on('connection', (socket) => {
             value: 0
         }
     );
-    io.emit('scores', scores);
+    socket.on('getStartingInfo',()=>{
+        currentPlaces=[];
+        io.emit('getCurrentPlace');
+
+        setTimeout(()=>{
+            const averagePlace = getAverage() ? getAverage() : 0;
+            startWord = Math.floor(averagePlace/100);  //the 100 is the speed, probably should change it to a var
+            delayTime = 100-averagePlace % 100;
+            io.to(clients[clients.length-1]).emit('startingInformation', {
+                scores: scores,
+                name: genNewName,
+                startWord: startWord,
+                delayTime: delayTime,
+                canBuzz: canBuzz,
+                question: currentQuestion
+            });
+        },100); //this wait is also to account for latency
+    });
+    
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 
+    socket.on('currentPlace', (currentPlace) => {
+        if(currentPlace!=null){
+            currentPlaces.push(currentPlace);
+        }
+    });
+
     socket.on('chatMessage', (message) => {
+        message.time=getTime();
         io.emit('chatMessage', message);
     });
 
     socket.on('buzz', (buzzInfo) => {
+        buzzes.push(buzzInfo);
         const name = buzzInfo.name;
-        const lastWord = buzzInfo.lastWord;
         const timeSince = buzzInfo.timeSince;
+        setTimeout(()=>{
+            buzzes.sort((a, b) => (a.timeSince > b.timeSince) ? 1 : -1)
+            if(canBuzz&&buzzes[0].name==name){
+                canBuzz=false;
+                io.emit('chatMessage',{
+                    content: name + " buzzed",
+                    time: getTime(),
+                    type: "notification"
+                });
+                io.emit('buzz',name)        
+            }
+        }, 50); //the 50 is how much time it waits, this should make it work if latency is under 50 milliseconds right now (also helps dealing with variance in setinterval)
     });
 
     socket.on('submitAnswer', (answerInfo) => {
         const name = answerInfo.name;
         const answer = answerInfo.answer;
-        const hours = answerInfo.hours;
-        const minutes = answerInfo.minutes;
-        console.log("answer: " + answer + " " + "correct answer: " + correctAnswer);
+ 
         if (answer==correctAnswer){
             io.emit('updateScore', {
-                name: name,
+                player: name,
                 scoreChange: 10
             });
             io.emit('chatMessage',{
                 content: name + "  for 10.",
-                time: "" + hours + ":" + (String(minutes).length==2 ? minutes:"0"+minutes),
+                time: getTime(),
                 type: "notification"
             });
 
@@ -91,7 +141,7 @@ io.on('connection', (socket) => {
 
             io.emit('chatMessage',{
                 content: name + " negs 5.",
-                time: "" + hours + ":" + (String(minutes).length==2 ? minutes:"0"+minutes),
+                time: getTime(),
                 type: "notification"
             });
 
@@ -104,17 +154,20 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('nextQuestion', () =>{
-        io.emit('nextQuestion',fetchNewQuestion());
+    socket.on('nextQuestion',()=>{
+        canBuzz=true;
+        time=new Date();
+        io.emit('nextQuestion',{
+            questionText: fetchNewQuestion()
+        });
     });
 
     socket.on('nameChange', (names) => {
         for (x of scores){
             if (x.player==names.oldName){
-                scoresID=scores.indexOf(x);
+                x.player=names.newName;
             }
         }
-        scores[scoresID].player=names.newName;
     });
 });
 
@@ -124,5 +177,15 @@ http.listen(3000, () => {
 
 function fetchNewQuestion(){
     correctAnswer = 'energy';
-    return("Because this quantity is conserved, perpetual motion machines of the first kind cannot exist. A capacitor stores this quantity in an electric field between its plates. The SI unit for this quantity is describes the work done when a force of one (*)) newton moves an object one meter. For ten points, name this physical quantity that is measured in Joules and comes in kinetic and potential forms.");
+    currentQuestion="Because this quantity is conserved, perpetual motion machines of the first kind cannot exist. A capacitor stores this quantity in an electric field between its plates. The SI unit for this quantity is describes the work done when a force of one (*)) newton moves an object one meter. For ten points, name this physical quantity that is measured in Joules and comes in kinetic and potential forms.";
+    return(currentQuestion);
 }
+
+
+function getTime(){
+    time = new Date();
+    hours=time.getHours();
+    minutes=time.getMinutes();
+    return("" + hours + ":" + (String(minutes).length==2 ? minutes:"0"+minutes));
+}
+
