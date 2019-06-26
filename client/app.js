df6235e656861a7c7d1c2a35f946a01d891e8414
biz11 <biz11@scasd.org>
2019-06-26 12:57:35 -0400

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,9 +1,4 @@
-// I was really inconsistent on using these btw
-const question = document.getElementById('question');
-const chat = document.getElementById('chat');
-const scoreboard = document.getElementById('scoreboard');
 let playerName=""
-
 let messages=[];
 let scores=[];
 let words;
@@ -14,10 +9,12 @@ var socket = io("http://127.0.0.1:3000/");
 let finishedQuestion=false;
 let delay=0;
 let writingQuestion;
-socket.on('chatMessage', (message) => {
-    messages.push(message);
-    updateChat();
-});
+
+
+
+window.onload = function startingInfo(){
+    socket.emit('getStartingInfo');
+}
 
 socket.on('startingInformation', (startingInformation) => {
     scores=startingInformation.scores;
@@ -26,13 +23,15 @@ socket.on('startingInformation', (startingInformation) => {
     delay=startingInformation.delayTime;
     document.getElementById('name').value=playerName;
     fullText=startingInformation.question;
+
+    //if someone has buzzed don't write the question and disable buttons
     if (startingInformation.canBuzz==false){
         document.getElementById("buzzButton").disabled = true;
         document.getElementById("nextButton").disabled = true;
-
         writingQuestion=false;
     }
     else{
+        //starts writing the question after the delay
         writingQuestion=true;
         lastWordTime=getTime();
         setTimeout(nextWord,delay);
@@ -40,77 +39,81 @@ socket.on('startingInformation', (startingInformation) => {
     updateScores();
 });
 
+socket.on('chatMessage', (message) => {
+    messages.push(message);
+    updateChat();
+});
+
+
 socket.on('updateScore', (scoreChange)=> {
-    let scoresID=0;
-    playerToChange=scoreChange.player;
-    console.log(playerToChange);
-    console.log(JSON.stringify(scores));
+    playerToChange=scoreChange.player;    
     changeAmount=scoreChange.scoreChange;
 
+    let scoresID=0;
+    //determines which element of score needs to be changed and changes it
     for (x of scores){
         if (x.player==playerToChange){
-            console.log("it noticed");
             scoresID=scores.indexOf(x);
         }
     }
     scores[scoresID].value+=changeAmount;
     updateScores();
 
+    //resets buttons
     document.getElementById("answer").value="";
     document.getElementById("answer").style.visibility = "hidden";
     document.getElementById("nextButton").disabled = false;
 
+    //if the answer was right, go to the end of the question
     if (changeAmount>0){
         finishedQuestion=true;
         words=fullText.length;
     }
+    //if the answer was wrong, continue with the question
     else{
         document.getElementById("buzzButton").disabled = false;
         lastWordTime=getTime();
         setTimeout(nextWord,speed);
         writingQuestion=true;
     }
-
 });
 
-window.onload = function startingInfo(){
-    messages =[
-    ];
-    socket.emit('getStartingInfo');
-}
 
 function nextQuestion(){
     socket.emit('nextQuestion');
 }
 
 socket.on('nextQuestion', (nextQuestion)=>{
+    fullText=nextQuestion.questionText;
     writingQuestion=true;
     finishedQuestion=false;
     words=0;
-    fullText=nextQuestion.questionText;
     document.getElementById("buzzButton").disabled = false;
     document.getElementById("nextButton").disabled = false;
-
     document.getElementById("answer").style.visibility = "hidden";
+
     lastWordTime=getTime();
     setTimeout(nextWord, speed);
 });
 
 
 function nextWord(){
-    time=new Date();
     const textToShow = fullText.split(" ").splice(0,words).join(" ");
     const remainingText = fullText.split(" ").splice(words).join(" ");
-    //console.log("getTime(): "+(getTime()-lastWordTime));
-    wordsChange=(Math.floor(getTime()-lastWordTime)/speed);
+
+    //calculates whether or not it is time to display the next word, and how long to wait before the next word -- to account for variance in setTimeout
+    wordsChange=(Math.floor((getTime()-lastWordTime)/speed));
     words+=wordsChange;
+    delay=(speed-((getTime()-lastWordTime)%100));
+    //if the next word was added, update when the lastWord was added
     if (wordsChange!=0){
         lastWordTime=getTime();
     }
-    delay=(speed-((getTime()-lastWordTime)%speed));
+
     questionShowing.innerHTML=textToShow;
     questionHidden.innerHTML=remainingText;
-    
+
+    //checks if the question is over
     if(fullText != "" && textToShow==fullText){
         writingQuestion=false;
         finishedQuestion=true;
@@ -118,14 +121,6 @@ function nextWord(){
     if (writingQuestion){
         setTimeout(nextWord, delay);
     }
-}
-
-function buzz(){
-    time=new Date();
-    socket.emit('buzz', {
-        name:playerName,
-        timeSince: (words*speed+getTime()-lastWordTime),
-    })
 }
 
 socket.on('buzz', (whoBuzzed) => {
@@ -137,10 +132,32 @@ socket.on('buzz', (whoBuzzed) => {
     writingQuestion=false;
 });
 
+function buzz(){
+    time=new Date();
+    socket.emit('buzz', {
+        name:playerName,
+        timeSince: (words*speed+getTime()-lastWordTime),
+    })
+}
+
 socket.on('getCurrentPlace',()=>{
     time = new Date();
     const place=(words*speed+getTime()-lastWordTime)
     socket.emit('currentPlace',place);
+});
+
+socket.on('addName',(newName)=>{
+    scores.push({player: newName, value:0});
+    updateScores();
+})
+
+socket.on('nameChange',(names)=>{
+   for (x of scores){
+       if (x.player==names.oldName){
+           x.player=names.newName;
+       }
+   }
+   updateScores();
 });
 
 function submitAnswer(){
@@ -150,6 +167,79 @@ function submitAnswer(){
         finishedQuestion: finishedQuestion,
         power: !fullText.split(" ").splice(0,words).join(" ").includes("(*)") //This doesn't work with IE, so should probably just write a function, but I'm lazy rn
     });
+}
+
+function submitText(){
+    const message={
+        author: playerName,
+        content: document.getElementById('textChat').value,
+        type: "chat"
+    };
+    socket.emit('chatMessage', message);
+}
+
+function updateChat(){
+	const messagesHTML = messages
+		.map(message => createMessageHTML(message))
+        .join('');
+    document.getElementById('chat').innerHTML = messagesHTML;
+}
+
+function updateScores(){
+	const scoresHTML = scores
+		.map(score => createScoreHTML(score))
+        .join('');
+    document.getElementById('scoreboard').innerHTML = scoresHTML;
+}
+
+
+ //Submits chats on enter
+document.getElementById('textChat').onkeydown = function(e){
+    if(e.keyCode == 13){
+        submitText();
+        document.getElementById('textChat').value="";
+    }
+ };
+
+//Submits answer on enter -- also need to add a timer like protobowl has
+document.getElementById('answer').onkeydown = function(e){
+    if(e.keyCode == 13){
+        submitAnswer();
+    }
+}; 
+
+ //changes name on enter
+ document.getElementById('name').onkeydown = function(e){
+    if(e.keyCode == 13){
+        const oldPlayerName=playerName;
+        playerName=document.getElementById('name').value;
+
+        if (legalName(playerName) && playerName!=oldPlayerName){
+            socket.emit('nameChange',{
+                newName: playerName,
+                oldName: oldPlayerName
+            });
+        }
+        else{
+            if (playerName!=oldPlayerName){
+                window.alert("name taken");
+            }
+        }
+    }
+ }; 
+
+function legalName(name){
+    for (scoreItem of scores){
+        if (scoreItem.player==name){
+            return(false);
+        }
+    }
+    return(true);
+}
+
+function getTime(){
+    time=new Date();
+    return(((time.getHours()*60+time.getMinutes())*60+time.getSeconds())*1000+time.getMilliseconds());
 }
 
 
@@ -185,92 +275,4 @@ function createScoreHTML(score){
         <span style="float: right">${score.value}</span>
         </div>
     `; 
-}
-
-function submitText(){
-    const message={
-        author: playerName,
-        content: document.getElementById('textChat').value,
-        type: "chat"
-    };
-    socket.emit('chatMessage', message);
-}
-
-
-
-function updateChat(){
-	const messagesHTML = messages
-		.map(message => createMessageHTML(message))
-        .join('');
-    chat.innerHTML = messagesHTML;
-}
-
-function updateScores(){
-	const scoresHTML = scores
-		.map(score => createScoreHTML(score))
-        .join('');
-    scoreboard.innerHTML = scoresHTML;
-}
-
-
- //Submits chats on enter
-document.getElementById('textChat').onkeydown = function(e){
-    if(e.keyCode == 13){
-        submitText();
-        document.getElementById('textChat').value="";
-    }
- };
-
- //Submits answer on enter -- also need to add a timer like protobowl has
- document.getElementById('answer').onkeydown = function(e){
-    if(e.keyCode == 13){
-        submitAnswer();
-    }
- }; 
-
- socket.on('addName',(newName)=>{
-     scores.push({player: newName, value:0});
-     updateScores();
- })
- socket.on('nameChange',(names)=>{
-    for (x of scores){
-        if (x.player==names.oldName){
-            x.player=names.newName;
-        }
-    }
-    updateScores();
- });
- //changes name on enter
- document.getElementById('name').onkeydown = function(e){
-    if(e.keyCode == 13){
-        const oldPlayerName=playerName;
-        playerName=document.getElementById('name').value;
-
-        if (legalName(playerName) && playerName!=oldPlayerName){
-            socket.emit('nameChange',{
-                newName: playerName,
-                oldName: oldPlayerName
-            });
-        }
-        else{
-            if (playerName!=oldPlayerName){
-                window.alert("name taken");
-            }
-        }
-    }
- }; 
-
- 
-function legalName(name){
-    for (scoreItem of scores){
-        if (scoreItem.player==name){
-            return(false);
-        }
-    }
-    return(true);
-}
-
-function getTime(){
-    time=new Date();
-    return(((time.getHours()*60+time.getMinutes())*60+time.getSeconds())*1000+time.getMilliseconds());
 }
