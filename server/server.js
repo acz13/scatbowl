edc53650656113ecccd8f4aedaf70a557e349970
biz11 <biz11@scasd.org>
2019-06-26 20:26:47 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,49 +5,44 @@ const io = require('Socket.io')(http)
 const path = require('path');
 const url = require('url');
 
-const clients=[];
+const clients={};
 let correctAnswer='';
 const scores=[];
 let scoresID=0;
 
-let buzzes=[];
+let buzzes={};
 let currentPlaces=[];
 let canBuzz=true;
 let startWord;
 let delayTime;
 let currentQuestion='';
+let roomJoined='';
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, + 'public/index.html'))
 });
 
-
-function legalName(name){
-    for (scoreItem of scores.roomJoined){
-        if (scoreItem.player==name){
-            return(false);
-        }
-    }
-    return(true);
-}
-function getAverage(){
-    let total=0;
-    for(place of currentPlaces){
-        total+=place;
-    }
-    return(total/currentPlaces.length);
-}
 io.on('connection', (socket) => {
-    let roomJoined=(url.parse(socket.handshake.headers.referer).query);
-    !('roomJoined' in clients) && (clients.roomJoined = []);
-    !('roomJoined' in scores) && (scores.roomJoined = []);
 
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    roomJoined=(url.parse(socket.handshake.headers.referer).query);
+    console.log(roomJoined);
+    console.log(clients);
     socket.join(roomJoined);
+    if (!(roomJoined in clients)) {
+        clients[roomJoined] = [];
+    };
+    if (!(roomJoined in scores)) {
+        scores[roomJoined] = [];
+    }
+    console.log(clients);
     if(currentQuestion==''){
         fetchNewQuestion();
     }
-    clients.roomJoined.push(socket.id);
+    clients[roomJoined].push(socket.id);
 
     let genNewName = "SCAT";
     let z = 1;
@@ -55,23 +50,25 @@ io.on('connection', (socket) => {
         genNewName="SCAT"+z;
         z+=1;
     }
-    scores.roomJoined.push(
+    scores[roomJoined].push(
         {
             player: genNewName,
             value: 0
         }
     );
+
     socket.on('getStartingInfo',()=>{
-        currentPlaces=[];
+        currentPlaces[roomJoined] = [];
         io.in(roomJoined).emit('getCurrentPlace');
         io.in(roomJoined).emit('addName',genNewName);
 
         setTimeout(()=>{
+            console.log(scores);
             const averagePlace = getAverage() ? getAverage() : 0;
             startWord = Math.floor(averagePlace/100);  //the 100 is the speed, probably should change it to a var
             delayTime = 100-averagePlace % 100;
             io.to(socket.id).emit('startingInformation', {
-                scores: scores.roomJoined,
+                scores: scores[roomJoined],
                 name: genNewName,
                 startWord: startWord,
                 delayTime: delayTime,
@@ -80,15 +77,10 @@ io.on('connection', (socket) => {
             });
         },100); //this wait is also to account for latency
     });
-    
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
 
     socket.on('currentPlace', (currentPlace) => {
-        if(currentPlace!=null){
-            currentPlaces.push(currentPlace);
+        if(currentPlace!=null){ //because the client requesting will have nothing
+            currentPlaces[roomJoined].push(currentPlace);
         }
     });
 
@@ -133,12 +125,12 @@ io.on('connection', (socket) => {
                 type: "notification"
             });
 
-            for (x of scores.roomJoined){
+            for (x of scores[roomJoined]){
                 if (x.player==name){
-                    scoresID=scores.roomJoined.indexOf(x);
+                    scoresID=scores[roomJoined].indexOf(x);
                 }
             }
-            scores.roomJoined[scoresID].value+=points;
+            scores[roomJoined][scoresID].value+=points;
         }
         else{
             const points=((finishedQuestion) ? 0 : -5);
@@ -154,12 +146,12 @@ io.on('connection', (socket) => {
                 type: "notification"
             });
 
-            for (x of scores.roomJoined){
+            for (x of scores[roomJoined]){
                 if (x.player==name){
-                    scoresID=scores.roomJoined.indexOf(x);
+                    scoresID=scores[roomJoined].indexOf(x);
                 }
             }
-            scores.roomJoined[scoresID].value+=points;
+            scores[roomJoined][scoresID].value+=points;
         }
     });
 
@@ -172,7 +164,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('nameChange', (names) => {
-        for (x of scores.roomJoined){
+        for (x of scores[roomJoined]){
             if (x.player==names.oldName){
                 x.player=names.newName;
             }
@@ -200,4 +192,20 @@ function getFormattedTime(){
     hours=time.getHours();
     minutes=time.getMinutes();
     return("" + hours + ":" + (String(minutes).length==2 ? minutes:"0"+minutes));
+}
+
+function legalName(name){
+    for (scoreItem of scores[roomJoined]){
+        if (scoreItem.player==name){
+            return(false);
+        }
+    }
+    return(true);
+}
+function getAverage(){
+    let total=0;
+    for(place of currentPlaces[roomJoined]){
+        total+=place;
+    }
+    return(total/currentPlaces[roomJoined].length);
 }
