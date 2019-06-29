1176e0a030545b61849f51c05727f8e8e35fe073
biz11 <biz11@scasd.org>
2019-06-29 11:51:39 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,18 +5,14 @@ const io = require('Socket.io')(http)
 const path = require('path');
 const url = require('url');
 
-const clients={};
-const scores=[];
-const speed = 100; //currently this is separate on the server and client, should probably make it part of the room properties and send it over when the user connects
-let scoresID=0;
 
-let buzzes=[];
-let currentPlaces=[];
-let canBuzz=true;
 let startWord;
 let delayTime;
-let currentQuestions=[];
 let roomJoined='';
+const roomProperties=[];
+const speed = 100;
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res) => {
@@ -35,42 +31,45 @@ io.on('connection', (socket) => {
     roomJoined=(url.parse(socket.handshake.headers.referer).query);
     socket.join(roomJoined);
 
-    //the short circuit form is still way nicer, but fine
-
-    
-    //initializes the room property to all of the server information
-    if (!(roomJoined in clients)) {
-        clients[roomJoined] = [];
-        scores[roomJoined] = [];
-        buzzes[roomJoined] = [];
-        currentQuestions[roomJoined] = fetchNewQuestion();
-    }
-
 
     let genNewName = "SCAT";
     let z = 1;
 
     //generates a new name that isn't in the room
     //at some point probably replace with a random name generator thing 
+    
+    
+    if(!(roomProperties.find(x => x.roomName==roomJoined))){
+        newQuestion = fetchNewQuestion();
+        roomProperties.push({
+                roomName: roomJoined,
+                clients: [],
+                scores: [],
+                buzzes: [],
+                currentQuestion: newQuestion.question,
+                currentAnswer: newQuestion.answer,
+                currentPlaces: [],
+                canBuzz: true,
+                speed: speed
+        })
+    }
+
     while (!legalName({name: genNewName,room: roomJoined})){
         genNewName="SCAT"+z;
         z+=1;
     }
 
-    //adds the new player to the scoreboard
-    scores[roomJoined].push(
-        {
-            player: genNewName,
-            value: 0
-        }
-    );
-    clients[roomJoined].push(socket.id);
 
+    pushProperty(roomJoined, 'clients', socket.id);
+    pushProperty(roomJoined, 'scores', {player: genNewName, value: 0});
+
+
+    //adds the new player to the scoreboard
 
     socket.on('getStartingInfo',(room)=>{
         const roomJoined=room;
-        //resets the roomJoined
-        currentPlaces[roomJoined] = [];
+        //resets the currentPlaces to find them again
+        setProperty(roomJoined,'currentPlaces',[]);
 
         io.in(roomJoined).emit('getCurrentPlace');
         // this will then send the currentPlace back to the server and add it to currentPlaces
@@ -84,21 +83,21 @@ io.on('connection', (socket) => {
             delayTime = speed-averagePlace % speed;
 
             io.to(socket.id).emit('startingInformation', {
-                scores: scores[roomJoined],
+                scores: getProperty(roomJoined, 'scores'),
                 name: genNewName,
                 startWord: startWord,
                 delayTime: delayTime,
-                canBuzz: canBuzz,
-                question: currentQuestions[roomJoined].question
+                canBuzz: getProperty(roomJoined, 'canBuzz'),
+                question: getProperty(roomJoined, 'currentQuestion')
             });
-        },100); //this wait is to account for latency
+        },10); //this wait is to account for latency
     });
 
     socket.on('currentPlace', (placeInfo) => {
         const roomJoined=placeInfo.room;
         const currentPlace=placeInfo.place;
         if(currentPlace!=null){ //because the client requesting will have nothing
-            currentPlaces[roomJoined].push(currentPlace);
+            pushProperty(roomJoined, 'currentPlaces', currentPlace);
         }
     });
 
@@ -112,17 +111,19 @@ io.on('connection', (socket) => {
 
     socket.on('buzz', (buzzInfo) => {
         const roomJoined=buzzInfo.room;
-        console.log(buzzes);
-        buzzes[roomJoined].push(buzzInfo);
         const name = buzzInfo.name;
+
+        pushProperty(roomJoined, 'buzzes', buzzInfo)
 
         setTimeout(()=>{
             //sorts by timeSince, smallest to largest, which is essentially where they buzzed and the time between words they buzzed
             //the lower timeSince is, the earlier the buzz was
-            buzzes[roomJoined].sort((a, b) => (a.timeSince > b.timeSince) ? 1 : -1)
+            const roomIndex = roomProperties.findIndex(room => room.roomName == roomJoined);
 
-            if(canBuzz&&buzzes[roomJoined][0].name==name){ //if no one else has buzzed yet and it was the first buzz
-                canBuzz=false;
+            roomProperties[roomIndex].buzzes.sort((a, b) => (a.timeSince > b.timeSince) ? 1 : -1)
+
+            if(getProperty(roomJoined, 'canBuzz') && getProperty(roomJoined, 'buzzes')[0].name==name){ //if no one else has buzzed yet and it was the first buzz
+                setProperty(roomJoined, 'canBuzz', false);
                 //sends a chat notification that there was a buzz
                 io.in(roomJoined).emit('chatMessage',{
                     content: name + " buzzed",
@@ -141,11 +142,12 @@ io.on('connection', (socket) => {
         const answer = answerInfo.answer;
         const finishedQuestion = answerInfo.finishedQuestion;
         const power = answerInfo.power;
-        canBuzz=true;
-        buzzes[roomJoined]=[];
+        setProperty(roomJoined, 'buzzes', []);
+        setProperty(roomJoined, 'canBuzz', true);
+
 
         //checks if the answer is right
-        if (answer==currentQuestions[roomJoined].answer){
+        if (answer==getProperty(roomJoined, 'currentAnswer')){
             const points=((power) ? 15 : 10);
 
             //updates client side score
@@ -161,14 +163,14 @@ io.on('connection', (socket) => {
                 type: "notification"
             });
 
-            //updates the serverside scores
-            for (x of scores[roomJoined]){
-                if (x.player==name){
-                    scoresID=scores[roomJoined].indexOf(x);
-                }
-            }
-            scores[roomJoined][scoresID].value+=points;
+            // finds the index of the room
+            const indexOfRoom = roomProperties.findIndex(x => x.roomName == roomJoined)
+            // finds the index of the score that needs to be changed
+            const indexOfScore = roomProperties[indexOfRoom].scores.findIndex(x => x.player == name)
+
+            roomProperties[indexOfRoom].scores[indexOfScore].value+=points;
         }
+
         //if the answer is wrong
         else{
             const points=((finishedQuestion) ? 0 : -5);
@@ -185,24 +187,24 @@ io.on('connection', (socket) => {
                 time: getFormattedTime(),
                 type: "notification"
             });
+            // finds the index of the room
+            const indexOfRoom = roomProperties.findIndex(x => x.roomName == roomJoined);
+            // finds the index of the score that needs to be changed
+            const indexOfScore = roomProperties[indexOfRoom].scores.findIndex(x => x.player == name)
 
-            //updates server side score
-            for (x of scores[roomJoined]){
-                if (x.player==name){
-                    scoresID=scores[roomJoined].indexOf(x);
-                }
-            }
-            scores[roomJoined][scoresID].value+=points;
+            roomProperties[indexOfRoom].scores[indexOfScore].value+=points;
         }
     });
 
     socket.on('nextQuestion',(room)=>{
         const roomJoined=room;
-        canBuzz=true;
-        currentQuestions[roomJoined] = fetchNewQuestion();
+        setProperty(roomJoined, 'canBuzz', true);
+        const newQuestion = fetchNewQuestion();
+        setProperty(roomJoined, 'currentQuestion', newQuestion.question);
+        setProperty(roomJoined, 'currentAnswer', newQuestion.answer);
 
         io.in(roomJoined).emit('nextQuestion', {
-            questionText: currentQuestions[roomJoined].question
+            questionText: getProperty(roomJoined, 'currentQuestion')
         });
     });
 
@@ -210,11 +212,13 @@ io.on('connection', (socket) => {
         const roomJoined=names.room;
 
         //updates the score data on the server side
-        for (x of scores[roomJoined]){
-            if (x.player==names.oldName){
-                x.player=names.newName;
-            }
-        }
+        
+        // finds the index of the room
+        const indexOfRoom = roomProperties.findIndex(x => x.roomName == roomJoined)
+        // finds the index of the score that needs to be changed
+        const indexOfScore = roomProperties[indexOfRoom].scores.findIndex(x => x.player == names.oldName)
+        // sets that elements player value to the new name
+        roomProperties[indexOfRoom].scores[indexOfScore].player=names.newName;
 
         //updates the score data on the client side
         io.in(roomJoined).emit('nameChange',{
@@ -242,7 +246,7 @@ function legalName(nameInfo){
     const roomJoined=nameInfo.room;
     const name=nameInfo.name;
     //checks if the name already exists in the room, returns false if it is, true if it isn't (true = can change name)
-    for (scoreItem of scores[roomJoined]){
+    for (scoreItem of getProperty(roomJoined, 'scores')){
         if (scoreItem.player==name){
             return(false);
         }
@@ -254,8 +258,22 @@ function legalName(nameInfo){
 function getAverage(room){
     const roomJoined=room;
     let total=0;
-    for(place of currentPlaces[roomJoined]){
+    for(place of getProperty(roomJoined, 'currentPlaces')){
         total+=place;
     }
-    return(total/currentPlaces[roomJoined].length);
+    return(total/getProperty(roomJoined, 'currentPlaces').length);
+}
+
+function pushProperty(room, property, update) {
+    index = roomProperties.findIndex(x => x.roomName == room);
+    roomProperties[index][property].push(update);
+}
+
+function setProperty(room, property, update) {
+    index = roomProperties.findIndex(x => x.roomName == room);
+    roomProperties[index][property]=update;
+}
+
+function getProperty(room, property){
+    return(roomProperties.find(x => x.roomName == room)[property]);
 }
