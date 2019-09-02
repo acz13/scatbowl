d0ee814519fa626a3b081addc5001a207a872f8f
Albert Zhang <alchzh@gmail.com>
2019-09-02 11:53:32 -0400

    move to Buefy

--- client/vue-client/README.md
+++ client/vue-client/README.md
@@ -1,4 +1,4 @@
-# scatbowl
+# vue-client
 
 ## Project setup
 ```
--- client/vue-client/src/main.js
+++ client/vue-client/src/main.js
@@ -3,15 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import vuetify from './plugins/vuetify'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import Buefy from 'buefy'
+import './assets/scss/app.scss'
+
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
