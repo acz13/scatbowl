59d7bef2e0dc18b05427f06b949a1cab0089658a
Albert Zhang <alchzh@gmail.com>
2019-09-05 04:32:10 +0000

    Revert purgecss changes

--- client/vue-client/vue.config.js
+++ client/vue-client/vue.config.js
@@ -1,14 +1,4 @@
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-const glob = require('glob')
-
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new PurgecssPlugin({
-        paths: glob.sync('./src/**/*.{vue,js}')
-      })
-    ]
-  },
   devServer: {
     disableHostCheck: true
   }
