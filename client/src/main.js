4c9f5faa68b64d718a3ccaacb2ccc7faca5c9b99
Albert Zhang <alchzh@gmail.com>
2019-09-07 22:25:44 +0000

    Unscope unread

--- client/src/main.js
+++ client/src/main.js
@@ -5,21 +5,12 @@ import store from './store'
 import './registerServiceWorker'
 import './assets/scss/app.scss'
 
-import Button from 'buefy/dist/components/button'
-import Collapse from 'buefy/dist/components/collapse'
-import Field from 'buefy/dist/components/field'
-import Slider from 'buefy/dist/components/slider'
-
 import VueCompositionApi from '@vue/composition-api'
 
 Vue.use(VueCompositionApi)
 
-Vue.use(Button)
-Vue.use(Collapse)
-Vue.use(Field)
-Vue.use(Slider)
-
 Vue.config.productionTip = false
+Vue.config.performance = true
 
 new Vue({
   router,
